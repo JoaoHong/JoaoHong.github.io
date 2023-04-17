c02e0dbdfa629b9e631f575ba2902f0ae9158c02
JoaoHong <107969352+JoaoHong@users.noreply.github.com>
2023-04-17 17:04:09 -0300

    commit

--- ConyGreen.DAO/IRepository/IRepositoryLogAcesso.cs
+++
@@ -1,9 +0,0 @@
-﻿using ConyGreen.DAO.DbContext;
-using ConyGreen.DAO.Models;
-
-namespace ConyGreen.DAO.IRepository
-{
-	public interface IRepositoryLogAcesso : IRepository<LogAcesso, MySqlDbContext>
-	{
-	}
-}
--- ConyGreen.DAO/Models/VM/ErrorVM.cs
+++
@@ -1,9 +0,0 @@
-namespace ConnectaDB.DAO.Models.Views
-{
-	public class ErrorVM
-	{
-		public int ErroCode { get; set; }
-		public string Titulo { get; set; }
-		public string Mensagem { get; set; }
-	}
-}
